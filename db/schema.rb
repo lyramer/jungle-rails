2582d1d959585084970c270c46f2efbcff01db7a
andy <andy.wynden@gmail.com>
2019-02-22 15:22:35 -0800

    added refined login and logout feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190222221024) do
+ActiveRecord::Schema.define(version: 20190222230842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20190222221024) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "lname"
+    t.string   "remember_digest"
   end
 
   add_foreign_key "line_items", "orders"
