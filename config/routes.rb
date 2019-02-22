fa32bfee38801d540168c17ef8b00c41a9d941fe
andy <andy.wynden@gmail.com>
2019-02-22 14:53:35 -0800

    added login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'products#index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
